55e78f7d772ed49a9da98c7e7b16bffde662b0e9
dvbeato <diogo.vecchiati@gmail.com>
2016-06-18 13:42:58 -0300

    adding more testes to sample-spring project

--- samples/sample-spring/src/test/java/com/example/spring/SampleSpringApplicationTests.java
+++ samples/sample-spring/src/test/java/com/example/spring/SampleSpringApplicationTests.java
@@ -1,6 +1,8 @@
 package com.example.spring;
 
+import com.example.spring.models.Agency;
 import com.example.spring.models.Person;
+import com.example.spring.templates.AgencyTemplate;
 import com.example.spring.templates.PersonTemplate;
 import org.junit.Before;
 import org.junit.Test;
@@ -16,24 +18,51 @@ import org.springframework.transaction.annotation.Transactional;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
+import java.util.List;
+
 import static org.assertj.core.api.Java6Assertions.assertThat;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = SampleSpringApplication.class)
 @ActiveProfiles("test")
+@Transactional
 public class SampleSpringApplicationTests {
 
     @Autowired
     private Factory factory;
 
     @Test
-    @Transactional
-    public void expectedToPersistAPerson() throws Exception {
+    public void expectedToPersistEntity() throws Exception {
         Person person = new PersonTemplate(factory).create();
 
         assertThat(person).isNotNull();
 
         assertThat(person.getId())
+                .isNotNull()
+                .isGreaterThan(0);
+    }
+
+    @Test
+    public void expectedToPersistMultiplesEntities() throws Exception {
+        List<Person> persons = new PersonTemplate(factory).create(10);
+
+        assertThat(persons)
+                .hasSize(10)
+                .extracting(Person::getId)
+                .doesNotContainNull();
+    }
+
+    @Test
+    public void expectedToPersistDependencies() throws Exception {
+        Agency agency = new AgencyTemplate(factory).create();
+
+        assertThat(agency).isNotNull();
+        assertThat(agency.getId())
+                .isNotNull()
+                .isGreaterThan(0);
+
+        assertThat(agency.getBank()).isNotNull();
+        assertThat(agency.getBank().getId())
                 .isNotNull()
                 .isGreaterThan(0);
     }
